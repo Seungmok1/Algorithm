f4c0aa4163e09972c68aa8e2ecebb4d8ae829a50
Seungmok1 <103080705+Seungmok1@users.noreply.github.com>
2023-10-30 09:34:50 +0900

    docs: update BOJ_16928/Main.java

--- Seungmok/BOJ_16928/Main.java
+++ Seungmok/BOJ_16928/Main.java
@@ -1,55 +1,53 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
+    static Map<Integer, Integer> map = new HashMap<>();
+    static boolean[] visited = new boolean[101];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter((new OutputStreamWriter(System.out)));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[101];
-        boolean[] visited = new boolean[101];
-        Map<Integer, Integer> ladder = new HashMap<>();
-        Map<Integer, Integer> snake = new HashMap<>();
-        Queue<Integer[]> q = new LinkedList<>();
-        for(int i=0;i<n;i++) {
+        for (int i = 0; i < n + m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            ladder.put(a, b);
+            map.put(a, b);
         }
-        for(int i=0;i<m;i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            snake.put(a, b);
-        }
+        bw.write(bfs() + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 
-        q.add(new Integer[] {1, 0});
+    static int bfs() {
+        Queue<Integer[]> q = new LinkedList<>();
+        q.add(new Integer[]{1, 0});
+
         while (!q.isEmpty()) {
             Integer[] cur = q.poll();
-            for(int i=1;i<=6;i++) {
+            for (int i = 1; i <= 6; i++) {
                 int next = cur[0] + i;
-                int fin;
-                if(next>100) continue;
-                if(snake.containsKey(next)){
-                    q.add(new Integer[] {snake.get(next), cur[1]+1});
-                    visited[snake.get(next)] = true;
-                    fin = snake.get(next);
-                } else if (ladder.containsKey(next)) {
-                    q.add(new Integer[] {ladder.get(next), cur[1]+1});
-                    visited[ladder.get(next)] = true;
-                } else if(!visited[next]){
-                    q.add(new Integer[]{next, cur[1]+1});
+                if (next > 100) continue;
+                if (map.containsKey(next)) {
+                    q.add(new Integer[]{map.get(next), cur[1] + 1});
+                    visited[map.get(next)] = true;
+                } else if (!visited[next]) {
+                    q.add(new Integer[]{next, cur[1] + 1});
                     visited[next] = true;
                 }
-                if(visited[100]) {
-                    System.out.println(cur[1]+1);
-                    return;
+                if (visited[100]) {
+                    return cur[1] + 1;
                 }
             }
         }
+        return -1;
     }
 }
